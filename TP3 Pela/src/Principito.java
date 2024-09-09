943e301c16672bf22258f53a550adb4cedad81a8
Thomas-Nichols <thomasnichols005@gmail.com>
2024-09-09 20:43:01 -0300

    El pala con el punto 3

--- TP3 Pela/src/Principito.java
+++ TP3 Pela/src/Principito.java
@@ -1,31 +1,51 @@
 package src;
+
 public class Principito {
     private String actitud;
     private String personalidad;
+    private Flor flor; // Flor que pertenece al principito
 
-    //Getters
-    public String getActitud () {
+    // Getters
+    public String getActitud() {
         return actitud;
     }
-    public String getPersonalidad () {
+
+    public String getPersonalidad() {
         return personalidad;
     }
 
-    //Setters
-    public void setActitud (String newActitud) {
+    public Flor getFlor() {
+        return flor;
+    }
+
+    // Setters
+    public void setActitud(String newActitud) {
         this.actitud = newActitud;
     }
-    public void setPersonalidad (String newPersonalidad) {
+
+    public void setPersonalidad(String newPersonalidad) {
         this.personalidad = newPersonalidad;
     }
 
+    public void setFlor(Flor flor) {
+        this.flor = flor;
+    }
 
-    
-    public Principito () {}
+    // Constructor vacío
+    public Principito() {
+    }
 
-    public Principito (String act, String perso) {
+    // Constructor con parámetros
+    public Principito(String act, String perso, Flor flor) {
         setActitud(act);
         setPersonalidad(perso);
+        setFlor(flor);
     }
-    
+
+    // Método para imprimir los atributos de la flor del principito
+    public void imprimirFlorPrincipito() {
+        System.out.println("La flor del principito tiene las siguientes características:");
+        System.out.println("Actitud: " + flor.getActitud());
+        System.out.println("Apariencia: " + flor.getApariencia());
+    }
 }
