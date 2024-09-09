c18fd16146d3cd2e83043c8ef4db7df9f83fac64
Felipe Iphar <ipharfelipe@gmail.com>
2024-09-09 19:10:24 -0300

    Inicio del repositorio, tp3 y tp4(sin empezar)

---
+++ TP3/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Flor florTexto = new Flor("vanidosa", "hermosa");
+        Flor florMia  = new Flor("generosa", "delicada");
+        /* 
+        Principito princTexto = new Principito ("Dedicado", "Explorador");
+        Principito princMio = new Principito ("Atento", "Calmado");
+        */
+        
+
+    }
+}
