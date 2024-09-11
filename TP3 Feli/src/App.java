f9480de7a25713c180bf83bbf1293d881307c323
Felipe Iphar <ipharfelipe@gmail.com>
2024-09-11 18:43:55 -0300

    Cambio de nombre

--- TP3 Feli/src/App.java
+++ TP3 Feli/src/App.java
@@ -8,7 +8,7 @@ public class App {
         
 
         System.out.println("Texto original");
-        System.out.println("El principito tenía una flor que amaba mucho. \nCuidaba de ella todos los días, la regaba y le quitaba las orugas. \nLa flor, aunque un poco vanidosa, era muy hermosa y agradecía al principito por su dedicación. \nUn dia, el principito decidió explorar otros planetas y, aunque no quería dejar sola a su flor, sabía que debía continuar su viaje para aprender más sobre el universo.");
+        System.out.println("El principito tenía una flor que amaba mucho. \nCuidaba de ella todos los días, la regaba y le quitaba las orugas. \nLa flor, aunque un poco " + florTexto.getActitud() +", era muy " + florTexto.getApariencia() +" y agradecía al principito por su dedicación. \nUn dia, el principito decidió explorar otros planetas y, aunque no quería dejar sola a su flor, sabía que debía continuar su viaje para aprender más sobre el universo.");
         System.out.println("");
         System.out.println("");
 
