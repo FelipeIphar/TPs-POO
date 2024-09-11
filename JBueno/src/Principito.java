3e344b1ca93e6f62686e20066cf83dbf414702c3
reseabalo <jeremiasadam12@gmail.com>
2024-09-11 20:39:03 -0300

    Ok TaTeTi

--- JBueno/src/Principito.java
+++ JBueno/src/Principito.java
@@ -2,7 +2,10 @@ public class Principito {
 
     private String Actitud;
     private String Personalidad;
+    private Flor FlorPrincipito; //esta flor pertenece al principito
 
+
+    //Getters
     public String getActitud(){
         return Actitud;
     }
@@ -11,6 +14,11 @@ public class Principito {
         return Personalidad;
     }
 
+    public Flor getFlor(){
+        return FlorPrincipito;
+    }
+
+    //Setters
     public void setActitud(String newActitud){
         this.Actitud = newActitud;
     }
@@ -19,13 +27,22 @@ public class Principito {
         this.Personalidad = newPersonalidad;
     }
 
-    public Principito(String Act, String Pers){
-        setActitud(Act);
-        setPersonalidad(Pers);
+    public void setFlor(Flor flor){
+        this.FlorPrincipito = flor;
     }
 
+    //Constructor Con una flor
+    public Principito(String Act, String Pers, Flor flor){
+        setActitud(Act);
+        setPersonalidad(Pers);
+        setFlor(flor);
+        
+    }
+
+    //Constructor Vacio
     public Principito(){
         
     }
+
     
 }
